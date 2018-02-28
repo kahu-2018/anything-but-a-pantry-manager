bb826f1ab828ab126281286abb611b5b492bd6ec
Maia <maiamiller25@gmail.com>
2018-03-01 10:57:53 +1300

    ClassNames to hide if not authenticated

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -4,9 +4,10 @@ const Nav = () => {
   return(
     <div>
       <div>Home</div>
-      <div>Profile</div>
-      <div>Find Recipe</div>
-      <div>Login</div>
+      <div className={isAuthenticated ? 'show' : 'hide'}>Profile</div>
+      <div className={isAuthenticated ? 'show' : 'hide'}>Find Recipe</div>
+      <div className={isAuthenticated ? 'hide' : 'show'}>Login</div>
+      <div className={isAuthenticated ? 'show' : 'hide'}>Logout</div>
     </div>
   )
 }
