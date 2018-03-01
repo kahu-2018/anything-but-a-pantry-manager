143ae1e4adc2673dec74c55c1e2a320e97399f5b
jessica draper <j.b.draper@hotmail.com>
2018-03-01 16:20:34 +1300

    FIRST TEST

---
+++ test/server/db/usersDb.test.js
@@ -0,0 +1,19 @@
+const env = require('../test-environment')
+const usersDb = require('../../../server/db/usersDb')
+
+let testDb = null
+
+beforeEach(() => {
+  testDb = env.getTestDb()
+  return env.initialise(testDb)
+})
+afterEach(() => env.cleanup(testDb))
+
+test('read users db', () => {
+  console.log(usersDb)
+  return usersDb.getUsers(testDb)
+    .then(users => {
+      expect(users.length).toBe(1)
+      expect(users[0].hasOwnProperty('first_name')).toBeTruthy()
+    })
+})
