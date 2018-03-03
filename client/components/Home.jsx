ed0772b609633bb3413c652505378508fcda9f0a
Melissa Mepham <m.a.mepham@gmail.com>
2018-03-03 16:21:56 +1300

    done homepage

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello World from Home</h1>
+        <img className='headerImage' src="images/pantry-manager-header.jpg" alt='header'/>
+        <div>
+          <div className="container-fluid full-width">
+            <div className="row">
+              <div className="col-sm-3">
+                <Link to='/login'><input className="btn btn-md btn-green btn-block mb-3" value="Login" type="submit" /></Link>
+              </div>
+              <div className="col-sm-3">
+                <Link to='/register'><input className="btn btn-md btn-green btn-block mb-3" value="Register" type="submit" /></Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
