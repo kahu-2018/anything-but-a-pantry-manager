5a4fc27c88db58d2e014d3fdf10ef5d490a8c320
Melissa Mepham <m.a.mepham@gmail.com>
2018-03-03 22:45:07 +1300

    done the profile page

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import {connect} from 'react-redux'
+
 import EditProfile from './EditProfile'
 
-import {getUserProfile} from '../actions/user'
+
+import { Link } from 'react-router-dom'
+
+import { getUserProfile } from '../actions/user'
 
 class Profile extends React.Component {
   constructor(props){
     super(props)
-    this.state={
+    this.state = {
       editVisible:false
+
     }
     this.toggleButton = this.toggleButton.bind(this)
   }
@@ -21,23 +26,55 @@ class Profile extends React.Component {
     const buttonText = this.state.editVisible ? 'Close' : 'Edit'
     return (
       <div>
-        <h1>Profile</h1>            
-        <button type="edit" className="btn btn-primary" onClick={this.toggleButton}>{buttonText}</button>
+        <img className='headerImage' src="images/pantry-to-plate-sml.jpg" alt='header'/>
+          <div className="container-fluid full-width">
+            <div className="row">
+              <div className='col-sm-3'>
+            </div>
+              <div className='col-sm-6'>
+            <h1 className='greenText'>Welcome Kubo Mepham{this.props.user.first_name} {this.props.user.last_name}</h1>
+            </div>
+            <div className='col-sm-3'>
+              <Link to='/editProfile'><input className="btn btn-md btn-green float-right" value="Edit" type="submit" /></Link>
+          </div>
+        </div>
 
-        {this.state.editVisible && <EditProfile />}
-        <h3>Welcome {this.props.user.first_name} {this.props.user.last_name}</h3>
+          <div className="row">
+              <div className="col-sm-3 centered">
+                <img className='profileImage' src='./images/kubz.jpg' alt='profile image'/>
+                <h3 className='greenText'>Kubz17{this.props.user.user_name}</h3>
+                <p>kubz-is-my-fav@gmail.com {this.props.user.email}</p>
+                <h3 className='greenText'>Friends</h3>
+                <input className="btn btn-lg btn-green btn-block mb-3" value="Go to Shopping List" type="submit" />
+                <input className="btn btn-lg btn-green btn-block mb-3" value="History" type="submit" />
+              </div>
 
-        <h3>Dietary Requirements:</h3> 
+              <div className="col-sm-3">
+                <input className="btn btn-lg btn-green btn-block mb-3" value="Favorite Recipes" type="submit" />
+                <input className="btn btn-sm btn-outline-green btn-block mb-3" type='submit' value='Apple Salad'/>
+                <input className="btn btn-sm btn-outline-green btn-block mb-3" type='submit' value='Raw Apple Pie'/>
+                <input className="btn btn-sm btn-outline-green btn-block mb-3" type='submit' value='Pizza Crusts'/>
+              </div>
 
-        <h3>Favourite Food:</h3> 
+              <div className="col-sm-3 centered">
+                <input className="btn btn-lg btn-green btn-block mb-3" value="Dietary Requirements" />
+                <p>Gluten Free <br /> Nut Allergy</p>
+                  <h4 className ='greenText'>I Love</h4>
+                  <p>Apples</p>
+                  <h4 className ='greenText'>I don't Like</h4>
+                  <p>Rice</p>
 
-        <h3>Favourite Recipes:</h3> 
-
-        <h3>Food I don't like:</h3> 
-
-        
-
-      </div>
+              </div>
+              <div className="col-sm-3 centered">
+                <input className="btn btn-lg btn-green btn-block mb-3" value="My Pantry"/>
+                <h4 className ='greenText'>Fresh</h4>
+                <p>Food</p>
+                <h4 className ='greenText'>Dairy</h4>
+                <p>Food</p>
+              </div>
+          </div>
+        </div>
+    </div>
     )
   }
 }
--- client/reducers/user.js
+++ client/reducers/user.js
@@ -1,17 +1,19 @@
 import request from '../utils/api'
 import { get } from '../utils/localstorage'
 
-// const initialState = {
-//   first_name: 'Kubo',
-//   last_name: 'Mepham',
-//   dietary_requirements: 'banana'
-// }
+const initialState = {
+  first_name: 'Kubo',
+  last_name: 'Mepham',
+  dietary_requirements: 'banana',
+  img: './images/kubz.jpg',
+  user_name: 'Kubs17'
+}
 
 function getUserProfile(state=[], action) {
     switch (action.type) {
       case 'RECIEVE_USER':
         return [...state, action.user.first_name]
-  
+
       default:
         return state
     }
