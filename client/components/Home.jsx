a3a93f53e99f24b9f0f155fec6cffd98dff44be3
Melissa Mepham <m.a.mepham@gmail.com>
2018-03-03 16:22:38 +1300

    yay

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -10,10 +10,10 @@ class Home extends React.Component {
           <div className="container-fluid full-width">
             <div className="row">
               <div className="col-sm-3">
-                <Link to='/login'><input className="btn btn-md btn-green btn-block mb-3" value="Login" type="submit" /></Link>
+                <Link to='/login'><input className="btn btn-lg btn-green btn-block mb-3" value="Login" type="submit" /></Link>
               </div>
               <div className="col-sm-3">
-                <Link to='/register'><input className="btn btn-md btn-green btn-block mb-3" value="Register" type="submit" /></Link>
+                <Link to='/register'><input className="btn btn-lg btn-green btn-block mb-3" value="Register" type="submit" /></Link>
               </div>
             </div>
           </div>
