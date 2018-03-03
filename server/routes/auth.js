88488d1ad840af8bcd18b9de4c88b1ddc4720a2c
YWHo <YWHo@users.noreply.github.com>
2018-03-03 16:10:14 +1300

    add login route

---
+++ server/routes/auth.js
@@ -0,0 +1,23 @@
+var router = require('express').Router()
+
+var {userExists, createUser} = require('../db/usersDb')
+var token = require('../auth/token')
+
+//router.post('/register', register, token.issue)
+
+// function register (req, res, next) {
+//   const {user_name, first_name, last_name, password, hourly_wage} = req.body
+//   userExists(user_name, req.app.get('db'))
+//     .then(exists => {
+//       if (exists) return res.status(400).send({message: "User Name Taken"})
+//       createUser(user_name, first_name, last_name, password, hourly_wage, req.app.get('db'))
+//         .then(() => next())
+//         .catch(err => res.status(500).send({message: "Server Error"}))
+//     })
+//     .catch(err => res.status(500).send({message: "Server Error"}))
+// }
+
+
+router.post('/login', token.verifyLogin)
+
+module.exports = router
