47f9ec56b262b4108b33ec7a2b1f111a3a256d25
Melissa Mepham <m.a.mepham@gmail.com>
2018-03-03 17:29:59 +1300

    log in page styled

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -36,7 +36,7 @@ const Nav = (props) => {
           </ul>
         <form className="form-inline my-2 my-lg-0">
           <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-blue my-2 my-sm-0" type="submit">Search</button>
+          <button className="btn btn-outline-green my-2 my-sm-0" type="submit">Search</button>
         </form>
       </div>
     </nav>
