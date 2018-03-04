d9d202421c3471080131a4223d16c266536d9415
YWHo <YWHo@users.noreply.github.com>
2018-03-04 22:36:15 +1300

    fix TypeError

--- client/actions/user.js
+++ client/actions/user.js
@@ -2,21 +2,13 @@ import request from '../utils/api'
 import {get} from '../utils/localstorage'
 
 
-// function receiveDietaryRestrictions(userDietaryRestrictions) {
-//   return {
-//     type: 'SET_RESTRICTIONS',
-//     userDietaryRestrictions
-//   }
-// }
-
 function receivedUser(user) {
   console.log("actions/user/receivedUser: user: ", user)
+  console.log("actions/user/receivedUser: if user: ", user)
   return {
     type: 'RECEIVED_USER',
-    user: { ...user
-    },
-    userDietaryRestrictions: { ...user.dietary_restrictions
-    }
+    user: {...user},
+    userDietaryRestrictions: {...user.dietary_restrictions}
   }
 }
 
@@ -24,17 +16,12 @@ function receivedDietaryRestriction(user) {
   console.log("actions/user/receivedDietaryRestriction: user: ", user)
   console.log("actions/user/receivedDietaryRestriction: user.dietary_restrictions: ", user.dietary_restrictions)
   const dietary_restrictions = user.dietary_restrictions
-  // if (!dietary_restrictions) {
-  //   return {
-  //     type: 'NOTHING'
-  //   }
-  // } else {
-    console.log("actions/user/receivedDietaryRestriction: dietary_restrictions: ", dietary_restrictions)
-    return {
-      type: 'RECEIVED_DR',
-      dietaryRestrictions: dietary_restrictions
-    }
- // }
+  console.log("actions/user/receivedDietaryRestriction: dietary_restrictions: ", dietary_restrictions)
+  return {
+    type: 'RECEIVED_DR',
+    dietaryRestrictions: dietary_restrictions
+  }
+
 }
 
 export function getUserProfile(userId) {
@@ -43,25 +30,16 @@ export function getUserProfile(userId) {
     const endpoint = 'users/' + userId
     request('get', endpoint)
       .then(res => {
-        dispatch(receivedUser(res.body.user))
-        dispatch(receivedDietaryRestriction(res.body.user))
+        if (res.body.user) {
+          dispatch(receivedUser(res.body.user))
+          dispatch(receivedDietaryRestriction(res.body.user))
+        }
       })
       .catch((err) => {
         console.log('actions/user/getUserProfile: error: ', err)
       })
   }
 }
-
-// export function getUserProfile(userId) {
-//   return (dispatch) => {
-//     console.log('user id', userId)
-//     request
-//       .get('/api/users/profile')
-//       .then(res => {
-//         console.log(res, "i am res")
-//       })
-//   }
-// }
 
 
 export const editProfileRequest = (newProfile) => {
