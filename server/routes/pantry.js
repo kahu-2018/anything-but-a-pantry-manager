acc124b184f2cb91af9116800f8f34351a05eccc
kahu2018 <fake@fake.com>
2018-03-06 11:29:50 +1300

    Action calls action.type

--- server/routes/pantry.js
+++ server/routes/pantry.js
@@ -4,10 +4,8 @@ const router = express.Router()
 const db = require('../db/pantryDb')
 
 router.get('/', (req, res) => {
-  console.log('route')
   db.getPantry()
   .then(ingredient => {
-    console.log('response', ingredient)
     res.json({pantry: ingredient})
   })
   .catch(err => {
