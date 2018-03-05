f027a1149a0575413624c222c9656681ef56aa51
Melissa Mepham <m.a.mepham@gmail.com>
2018-03-06 11:36:56 +1300

    edit profile working better

--- client/components/EditProfile.jsx
+++ client/components/EditProfile.jsx
@@ -3,6 +3,12 @@ import {connect} from 'react-redux'
 
 import {editProfileRequest} from '../actions/user'
 
+import profile from './Profile'
+import { Link } from 'react-router-dom'
+
+import { getUserProfile } from '../actions/user'
+import request from '../utils/api'
+
 class EditProfile extends React.Component {
   constructor(props) {
     super(props)
@@ -27,35 +33,50 @@ class EditProfile extends React.Component {
   render() {
     return (
       <div>
-        <form>
-          <img className='headerImage' src="images/pantry-to-plate-sml.jpg" alt='header'/>
-          <div className="col-sm-3">
-            <label className="first_name font-p">First name:</label>
-            <input type="first_name" className="form-control font-pLato backgroundForm" id="first_name"/>
-          </div>
-          <div className="col-sm-3">
-            <label className="last_name font-p">Last name:</label>
-            <input type="last_name " className="form-control font-pLato backgroundForm" id="last_name"/>
-          </div>
+        <img className='headerImage' src="images/pantry-to-plate-sml.jpg" alt='header'/>
 
-          <div className="col-sm-3">
-            <h4>Dietary Restrictions</h4>
-            {this.state.dietaryRestrictions.map(item => {
-              return <div className="checkbox">
-                <label><input type="checkbox" value=""/>{item}</label>
+          <div className="container-fluid full-width">
+            <div className="row">
+              <div className='col-sm-3'>
               </div>
-            })
-            }
-            </div>
-          </form>
-            <button type="edit" className="btn btn-lg btn-green btn-block text-center center-column mb-3" onClick={this.submitEdit}>Submit</button>
+              <div className='col-sm-6'>
+                <h1 className='greenText'>Welcome {this.props.user.first_name} {this.props.user.last_name}</h1>
+              </div>
+              <div className='col-sm-3'>
+                <Link to='/profile'><input className="btn btn-md btn-green float-right" value="Edit" type="submit" /></Link>
+          </div>
+        </div>
+            <div className="row">
 
+              <div className="col-sm-3">
+              <form>
+                  <label className="first_name font-p">First name:</label>
+                  <input type="first_name" className="form-control font-pLato backgroundForm" id="first_name"/>
+                  <label className="last_name font-p">Last name:</label>
+                  <input type="last_name " className="form-control font-pLato backgroundForm" id="last_name"/>
+
+                <h4>Dietary Restrictions</h4>
+                {this.state.dietaryRestrictions.map(item => {
+                  return <div className="checkbox">
+                  <label><input type="checkbox" value=""/>{item}</label>
+                  </div>
+                })
+              }
+              <button type="edit" className="btn btn-lg btn-green btn-block text-center center-column mb-3" onClick={this.submitEdit}>Submit</button>
+            </form>
+          </div>
+
+          </div>
+        </div>
       </div>
     )
   }
 }
-const mapStateToProps = ({userDetails}) => {
-  return {userDetails}
+const mapStateToProps = (state) => {
+  return {
+    auth: state.auth,
+    user: state.user,
+    dietaryRestrictions: state.dietaryRestrictions
+  }
 }
-
 export default connect(mapStateToProps)(EditProfile)
