02b92f61bbc1a7891aeee5115046bf32c2a57fec
YWHo <YWHo@users.noreply.github.com>
2018-03-06 01:58:23 +1300

    simple form validation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,8 +7,31 @@ router.post('/register', register)
 
 function register (req, res) {
     console.log('server/routes/auth: req.body: ', req.body)
+    if (!req || !req.body ) {
+        return res.status(400).send({message: "Invalid data"})
+    }
 
     const { first_name, last_name, user_name, email, password, confirm_password } = req.body
+
+    if (!first_name || !last_name || !user_name || !email || !password || !confirm_password) {
+        return res.status(400).send({message: "Incomplete fields"})
+    }
+
+    if ( !user_name.match(/^\w+$/)) {
+        return res.status(400).send({message: "Use only alphanumeric in username"})
+    }
+
+    if (password.length < 8) {
+        return res.status(400).send({message: "Password must be at least 8 characters"})
+    }
+
+    if (isCommonPassword(password)) {
+        return res.status(400).send({message: "Password is too common"})
+    }
+
+    if (password !== confirm_password) {
+        return res.status(400).send({message: "Passwords do not match"})
+    }
 
     userExists(user_name, email)
         .then(users => {
@@ -30,6 +53,42 @@ function register (req, res) {
         .catch(err => res.status(500).send({message: "Server Error"}))
 
 }
+
+function isCommonPassword(password) {
+    switch (password) {
+        case '123456789':
+        case '12345678':
+        case '1234567890':
+        case 'password':
+        case '987654321':
+        case 'qwertyuiop':
+        case 'qwertyuiop':
+        case '77777777':
+        case '1q2w3e4r':
+        case '3rjs1la7qe':
+        case '1q2w3e4r5t':
+        case 'letmein':
+        case 'football':
+        case 'iloveyou':
+        case 'starwars':
+        case 'passw0rd':
+        case 'password1':
+        case 'freedom':
+        case 'whatever':
+        case 'trustno1':
+        case 'sunshine':
+        case 'princess':
+        case 'superman':
+        case 'baseball':
+        case 'adobe123':
+        case 'Football':
+        case 'zaq1zaq1':
+            return true;
+        default:
+            return false;
+    }
+}
+
 
 
     //usernameExists
