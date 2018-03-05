0789fcaf85aa5d5f159e36f07b51eff3eb96f49c
nick-jac <nickinoz3@gmail.com>
2018-03-05 17:43:05 +1300

    update Heroku

--- client/components/EditProfile.jsx
+++ client/components/EditProfile.jsx
@@ -3,7 +3,6 @@ import {connect} from 'react-redux'
 
 import {editProfileRequest} from '../actions/user'
 
-
 class EditProfile extends React.Component {
   constructor(props) {
     super(props)
@@ -14,7 +13,7 @@ class EditProfile extends React.Component {
     this.updateProfileDetails = this.updateProfileDetails.bind(this)
     this.submitEdit = this.submitEdit.bind(this)
   }
-  updateProfileDetails (event) {
+  updateProfileDetails(event) {
     let {newProfile} = this.state
     newProfile[event.target.name] = event.target.value
     this.setState({newProfile})
@@ -31,30 +30,24 @@ class EditProfile extends React.Component {
           <img className='headerImage' src="images/pantry-to-plate-sml.jpg" alt='header'/>
           <div className="col-sm-3">
             <label className="first_name font-p">First name:</label>
-            <input type="first_name" className="form-control font-pLato backgroundForm" id="first_name" />
+            <input type="first_name" className="form-control font-pLato backgroundForm" id="first_name"/>
           </div>
           <div className="col-sm-3">
             <label className="last_name font-p">Last name:</label>
-            <input type="last_name " className="form-control font-pLato backgroundForm" id="last_name" />
+            <input type="last_name " className="form-control font-pLato backgroundForm" id="last_name"/>
           </div>
 
           <div className="col-sm-3">
             <label className="dietary_restrictions font-p">Dietary requirements:</label>
-            <input type="dietary_restrictions" className="form-control font-pLato backgroundForm" id="dietary_restrictions" />
+            <input type="dietary_restrictions" className="form-control font-pLato backgroundForm" id="dietary_restrictions"/>
 
           </div>
-            <button type="edit" className="btn btn-lg btn-green btn-block text-center center-column mb-3" onClick={this.submitEdit}>Submit</button>
-          <br />
-          <br />
-       </div>
-       <div className="row justify-content-around">
-
-  </div>
-</div>
-</div>
-</div>
-</form>
-</div>
+          <button type="edit" className="btn btn-lg btn-green btn-block text-center center-column mb-3" onClick={this.submitEdit}>Submit</button>
+          <br/>
+          <br/>
+        <div className="row justify-content-around"></div>
+      </form>
+    </div>
     )
   }
 }
--- client/components/ShoppingList.jsx
+++ client/components/ShoppingList.jsx
@@ -47,7 +47,7 @@ const mapStateToProps = (props) => {
     //   {"title":"Onion and Fresh Herb Omelet with Mixed Greens","href":"http://find.myrecipes.com/recipes/recipefinder.dyn?action=displayRecipe&recipe_id=1622444","ingredients":"egg substitute, milk, parsley, thyme, salt, black pepper, eggs, flour, nonstick cooking spray, onions, garlic, salad greens, salad greens, red wine vinegar, olive oil, goat cheese, almonds","thumbnail":"http://img.recipepuppy.com/514820.jpg"},
     //   {"title":"Spanish Omelet","href":"http://www.cooks.com/rec/view/0,185,153160-249194,00.html","ingredients":"vegetable oil, green pepper, onions, water, milk, eggs, black pepper, mushroom, garlic, salt, chili powder","thumbnail":""},
     //   {"title":"Picnic Omelet Squares Recipe","href":"http://www.cdkitchen.com/recipes/recs/2184/Picnic-Omelet-Squares99498.shtml","ingredients":"eggs, garlic, parmesan cheese, olive oil, onions, peas, potato, red pepper, salt, tomato, zucchini","thumbnail":""}
-    ]
+    //]
   }
 }
 
