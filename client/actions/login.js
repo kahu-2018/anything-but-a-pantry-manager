6bbd1cec8c338b0ebe7291f54e4dba77502ebbde
YWHo <YWHo@users.noreply.github.com>
2018-03-05 23:05:11 +1300

    remove unnecessary paremeter from calling of requestLogin

--- client/actions/login.js
+++ client/actions/login.js
@@ -31,7 +31,7 @@ export function loginRetype () {
 
 export function loginUser (creds) {
   return dispatch => {
-    dispatch(requestLogin(creds))
+    dispatch(requestLogin())
     return request('post', 'auth/login', creds)
       .then((response) => {
         const userInfo = saveUserToken(response.body.token)
