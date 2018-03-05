86a80ef7ec3f8919484a42e37e4f819b34cb22f6
kahu2018 <fake@fake.com>
2018-03-05 16:51:47 +1300

    Remove unnecessary comments and hardcode

--- client/components/ShoppingList.jsx
+++ client/components/ShoppingList.jsx
@@ -23,11 +23,6 @@ function ShoppingList(props) {
 //Transform count object into array
   let ingredientList= Object.keys(count).map((ingredient) => ({ingredient: ingredient, count: count[ingredient]}))
 
-
-  console.log('count', count)
-  console.log('noDuplicates', noDuplicates)
-
-
   return (
     <div>
       <h1>My Shopping List</h1>
@@ -36,7 +31,7 @@ function ShoppingList(props) {
       </div>
 
       <div className={(props.recipes == 0) ? 'show' : 'hide' }>
-        <p>No recipes selected for shoppling list!</p>
+        <p>No recipes selected for shopping list!</p>
       </div>
     </div>
   )
@@ -48,11 +43,11 @@ const mapStateToProps = (props) => {
 
   return {
     auth: props.auth,
-    // recipes: props.recipes
-    recipes: [
-      {"title":"Onion and Fresh Herb Omelet with Mixed Greens","href":"http://find.myrecipes.com/recipes/recipefinder.dyn?action=displayRecipe&recipe_id=1622444","ingredients":"egg substitute, milk, parsley, thyme, salt, black pepper, eggs, flour, nonstick cooking spray, onions, garlic, salad greens, salad greens, red wine vinegar, olive oil, goat cheese, almonds","thumbnail":"http://img.recipepuppy.com/514820.jpg"},
-      {"title":"Spanish Omelet","href":"http://www.cooks.com/rec/view/0,185,153160-249194,00.html","ingredients":"vegetable oil, green pepper, onions, water, milk, eggs, black pepper, mushroom, garlic, salt, chili powder","thumbnail":""},
-      {"title":"Picnic Omelet Squares Recipe","href":"http://www.cdkitchen.com/recipes/recs/2184/Picnic-Omelet-Squares99498.shtml","ingredients":"eggs, garlic, parmesan cheese, olive oil, onions, peas, potato, red pepper, salt, tomato, zucchini","thumbnail":""}
+    recipes: props.recipes
+    // recipes: [
+    //   {"title":"Onion and Fresh Herb Omelet with Mixed Greens","href":"http://find.myrecipes.com/recipes/recipefinder.dyn?action=displayRecipe&recipe_id=1622444","ingredients":"egg substitute, milk, parsley, thyme, salt, black pepper, eggs, flour, nonstick cooking spray, onions, garlic, salad greens, salad greens, red wine vinegar, olive oil, goat cheese, almonds","thumbnail":"http://img.recipepuppy.com/514820.jpg"},
+    //   {"title":"Spanish Omelet","href":"http://www.cooks.com/rec/view/0,185,153160-249194,00.html","ingredients":"vegetable oil, green pepper, onions, water, milk, eggs, black pepper, mushroom, garlic, salt, chili powder","thumbnail":""},
+    //   {"title":"Picnic Omelet Squares Recipe","href":"http://www.cdkitchen.com/recipes/recs/2184/Picnic-Omelet-Squares99498.shtml","ingredients":"eggs, garlic, parmesan cheese, olive oil, onions, peas, potato, red pepper, salt, tomato, zucchini","thumbnail":""}
     ]
   }
 }
