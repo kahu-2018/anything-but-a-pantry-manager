d3036c3596c6ea13b5bfbd345e2cda94255fb622
YWHo <YWHo@users.noreply.github.com>
2018-03-05 23:57:58 +1300

    backup

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,9 +3,13 @@ var router = require('express').Router()
 var {userExists, createUser} = require('../db/usersDb')
 var token = require('../auth/token')
 
-//router.post('/register', register, token.issue)
+router.post('/register', register)
 
-// function register (req, res, next) {
+function register (req, res) {
+    console.log(req.body)
+    res.status(200).json({
+        message: 'Account successfully created'
+    })
 //   const {user_name, first_name, last_name, password, hourly_wage} = req.body
 //   userExists(user_name, req.app.get('db'))
 //     .then(exists => {
@@ -15,7 +19,7 @@ var token = require('../auth/token')
 //         .catch(err => res.status(500).send({message: "Server Error"}))
 //     })
 //     .catch(err => res.status(500).send({message: "Server Error"}))
-// }
+}
 
 
 router.post('/login', token.verifyLogin)
