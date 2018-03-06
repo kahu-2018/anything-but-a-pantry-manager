1d86ca28a869db4e79a3409aa85527beed8658e1
YWHo <YWHo@users.noreply.github.com>
2018-03-06 14:53:35 +1300

    format indentation

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import {HashRouter as Router, Route, Link} from 'react-router-dom'
-import {connect} from 'react-redux'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 
 import Login from './Login'
@@ -19,62 +19,62 @@ class Nav extends React.Component {
     }
   }
 
-render() {
-  const { auth } = this.props
+  render() {
+    const { auth } = this.props
 
-  return (
-    <Router>
-      <div>
-      <nav className="navbar navbar-expand-md navbar-light bg-light navBar textColourNav">
-        <Link to='/' className="navbar-brand"><i className="fas fa-home faFaFont"></i></Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+    return (
+      <Router>
+        <div>
+          <nav className="navbar navbar-expand-md navbar-light bg-light navBar textColourNav">
+            <Link to='/' className="navbar-brand"><i className="fas fa-home faFaFont"></i></Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link to='/profile' className="nav-link">Profile </Link>
-              </li>
-              <li className="nav-item">
-                <Link to='/generateRecipe' className="nav-link">Generate Recipe</Link>
-              </li>
-              <li className="nav-item">
-                <Link to='/shoppinglist' className="nav-link">Shopping List</Link>
-              </li>
-        </ul>
-          <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link to='/login' className="nav-link"><i className="fas fa-user-circle faFaFont"></i>{auth.isAuthenticated ? "Logout" : "login/Register"}</Link>
-            </li>
-          </ul>
-        <form className="form-inline my-2 my-lg-0">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-green my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <Link to='/profile' className="nav-link">Profile </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to='/generateRecipe' className="nav-link">Generate Recipe</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to='/shoppinglist' className="nav-link">Shopping List</Link>
+                </li>
+              </ul>
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link to='/login' className="nav-link"><i className="fas fa-user-circle faFaFont"></i>{auth.isAuthenticated ? "Logout" : "login/Register"}</Link>
+                </li>
+              </ul>
+              <form className="form-inline my-2 my-lg-0">
+                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-green my-2 my-sm-0" type="submit">Search</button>
+              </form>
+            </div>
+          </nav>
 
-    <Route exact path='/' component={Home} />
-    <Route exact path='/login' component={Login} />
-    <Route exact path='/profile' component={Profile} />
-    <Route exact path='/generateRecipe' component={GenerateRecipe} />
-    <Route exact path='/recipe' component={Recipe} />
-    <Route exact path='/register' component={Register} />
-    <Route exact path='/editProfile' component={EditProfile} />
-    <Route exact path='/shoppinglist' component={ShoppingList} />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/profile' component={Profile} />
+          <Route exact path='/generateRecipe' component={GenerateRecipe} />
+          <Route exact path='/recipe' component={Recipe} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/editProfile' component={EditProfile} />
+          <Route exact path='/shoppinglist' component={ShoppingList} />
 
-    </div>
+        </div>
 
-  </Router>
-  )
-}
+      </Router>
+    )
+  }
 }
 
 const mapStateToProps = ({ auth }) => {
-    return {
-        auth
-    }
+  return {
+    auth
+  }
 }
 
 export default connect(mapStateToProps)(Nav)
