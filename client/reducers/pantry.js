8244dcf1980f57591fe24a5d56a02be6532da5c8
Maia <32394837+maia-miller@users.noreply.github.com>
2018-03-07 09:45:55 +1300

    Update pantry.js

--- client/reducers/pantry.js
+++ client/reducers/pantry.js
@@ -2,11 +2,9 @@ function receivePantry(state=[], action) {
   let newState = [...state]
   switch(action.type) {
     case 'RECEIVE_PANTRY':
-      console.log(action)
       return [...action.pantry]
 
       case 'DELETE_ITEM':
-      console.log(action, newState)
         return [...newState].filter(ingredient => ingredient.id !== action.ingredient.id)
 
     default:
