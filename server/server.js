a28b054fd4d4c8c8f828477451fee317ef7b4545
YWHo <YWHo@users.noreply.github.com>
2018-03-07 14:34:11 +1300

    enable redirect to https

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const server = express()
 
 server.use(cors('*'))
 
+server.use(requireHTTPS)
 server.use(bodyParser.json())
 server.use(express.static(path.join(__dirname, '../public')))
 
@@ -19,5 +20,19 @@ server.use('/api/recipes', generateRecipe)
 server.use('/api/users', users)
 server.use('/api/auth', auth)
 server.use('/api/pantry', pantry)
+
+
+// Source: 
+// 1. https://stackoverflow.com/questions/15813677/https-redirection-for-all-routes-node-js-express-security-concerns
+// 2. https://stackoverflow.com/questions/7185074/heroku-nodejs-http-to-https-ssl-forced-redirect
+function requireHTTPS(req, res, next) {
+    if ((process.env.NODE_ENV === 'production') &&
+        (req.headers['x-forwarded-proto'] !== 'https')) {
+        let secureUrl = 'https://' + req.get('host') + req.url
+        res.redirect(secureUrl)
+    } else {
+        next();
+    }
+}
 
 module.exports = server
